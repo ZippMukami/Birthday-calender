a342838a41c2d32083b64b5763f3a97a5a02da75
ZippMukami <zimukami@gmail.com>
2022-03-06 16:45:03 +0300

    This commit hides the landing page to show the form/second page.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let landing_page = document.querySelector(".landing-page");
+let fill_form = document.getElementById('fill-form');
+let results = document.getElementById('results');
+let btn1 = document.querySelector('.button');
+
+
+btn1.addEventListener("click", function(){
+    landing_page.style.display = "none";
+    fill_form.style.display = "block"
+});
